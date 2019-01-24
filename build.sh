1bc4b4e967fb3474a725c7f027f8669edfb308cb
veo.chen <veo.chen@voyager.nz>
2019-01-25 11:10:21 +1300

    add cross build

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-go build --ldflags="-s -w" -o dc
+env GOOS=linux GOARCH=amd64 go build --ldflags="-s -w" -o dc.linux
+env GOOS=darwin GOARCH=amd64 go build --ldflags="-s -w" -o dc.osx
+env GOOS=windows GOARCH=amd64 go build --ldflags="-s -w" -o dc.exe
